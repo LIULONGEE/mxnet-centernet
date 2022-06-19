520447a247d523e5c4a7e373e003eb46eb7f238b
LIULONGEE <36914379+LIULONGEE@users.noreply.github.com>
2022-06-19 16:37:23 +0800

    Update coco_centernet.py

--- dataset/coco_centernet.py
+++ dataset/coco_centernet.py
@@ -10,13 +10,75 @@ from utils.image import get_affine_transform, affine_transform
 from utils.image import gaussian_radius, draw_umich_gaussian, draw_msra_gaussian
 from utils.image import draw_dense_reg
 
+import pycocotools.coco as coco
+from pycocotools.cocoeval import COCOeval
 
-from dataset.coco import COCO
+import mxnet.gluon.data as data
 
-class CenterCOCODataset(COCO):
-    def __init__(self, opt, coco_path):
-        super(CenterCOCODataset, self).__init__(opt)
+class COCO(data.Dataset):
+ 
+  def __init__(self, opt, split):
+    super(COCO, self).__init__()
+    self.data_dir = os.path.join(opt.data_dir, 'coco')
+    self.img_dir = os.path.join(self.data_dir, '{}2017'.format(split))
+    if split == 'test':
+      self.annot_path = os.path.join(
+          self.data_dir, 'annotations',
+          'image_info_test-dev2017.json').format(split)
+    else:
+      if opt.task == 'exdet':
+        self.annot_path = os.path.join(
+          self.data_dir, 'annotations',
+          'instances_extreme_{}2017.json').format(split)
+      else:
+        self.annot_path = os.path.join(
+          self.data_dir, 'annotations',
+          'instances_{}2017.json').format(split)
     
+    
+  
+
+
+
+class CenterCOCODataset():
+     num_classes = 80
+     default_resolution = [512, 512]
+     mean = np.array([0.40789654, 0.44719302, 0.47026115],
+                   dtype=np.float32).reshape(1, 1, 3)
+     std  = np.array([0.28863828, 0.27408164, 0.27809835],
+                   dtype=np.float32).reshape(1, 1, 3)
+     class_name = [
+        '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+        'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+        'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+        'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
+        'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
+        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+        'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+        'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+        'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+        'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+     valid_ids = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
+        14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+        24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
+        37, 38, 39, 40, 41, 42, 43, 44, 46, 47,
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+        58, 59, 60, 61, 62, 63, 64, 65, 67, 70,
+        72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
+        82, 84, 85, 86, 87, 88, 89, 90]
+
+    def __init__(self, opt, coco_path, split):
+        super(CenterCOCODataset, self).__init__()
+        self.split = split
+        self.opt = opt
+        self.json_id_to_contiguous = None
+        self.contiguous_id_to_json = None
+        
+        print('==> initializing coco 2017 {} data.'.format(split))
         images = []
         categories = []
         # category_id is from 1 for coco, not 0
@@ -24,7 +86,6 @@ class CenterCOCODataset(COCO):
             categories.append({'supercategory':'none',
                               'id': i+1,
                               'name': name})
-
         annotations = []
         instance_counter = 1
         image_counter = 1
@@ -35,8 +96,6 @@ class CenterCOCODataset(COCO):
         for line in lines:
             # split any white space
             img_path, ann_path = line.strip().split()
-            img_path = osp.join(self.data_root, self.img_prefix, img_path)
-            ann_path = osp.join(self.data_root, self.ann_prefix, ann_path)
             # img = Image.open(img_path)
             # width, height = img.size
             width, height = imagesize.get(img_path)
@@ -68,30 +127,31 @@ class CenterCOCODataset(COCO):
         self.coco.dataset['images']=images
         self.coco.dataset['annotations']=annotations
         self.coco.createIndex()
+        
+        self.cat_ids = {v: i for i, v in enumerate(self._valid_ids)}
+        self.voc_color = [(v // 32 * 64 + 64, (v // 8) % 4 * 64, v % 8 * 32) \
+                      for v in range(1, self.num_classes + 1)]
+        self._data_rng = np.random.RandomState(123)
+        self._eig_val = np.array([0.2141788, 0.01817699, 0.00341571],
+                             dtype=np.float32)
+        self._eig_vec = np.array([
+        [-0.58752847, -0.69563484, 0.41340352],
+        [-0.5832747, 0.00994535, -0.81221408],
+        [-0.56089297, 0.71832671, 0.41158938]
+        ], dtype=np.float32)
 
-        ### mmdetection coco init
-        # avoid the filter problem in CocoDataset, view coco_api.py for detail
-        self.coco.img_ann_map = self.coco.imgToAnns
-        self.coco.cat_img_map = self.coco.catToImgs
+        
+        self.images = self.coco.getImgIds()
+        self.num_samples = len(self.images)
+        self.max_objs = 128
 
-        # get valid category_id (in annotation, start from 1, arbitary)
-        self.cat_ids = self.coco.get_cat_ids(cat_names=self.CLASSES)
-        # convert category_id to label(train_id, start from 0)
-        self.cat2label = {cat_id: i for i, cat_id in enumerate(self.cat_ids)}
-        self.img_ids = self.coco.get_img_ids()
-        # self.img_ids = list(self.coco.imgs.keys())
-        assert len(self.img_ids) > 0, 'image number must > 0'
-        N=len(self.img_ids)
-        print(f'load {N} image from YMIR dataset')
+        print('Loaded {} {} samples'.format(split, self.num_samples))
         
         self._coco = []
         self._load_jsons()
         self.classes = self.class_name[1:]
         
-    def get_ann_path_from_img_path(self,img_path):
-        img_id=osp.splitext(osp.basename(img_path))[0]
-        return osp.join(self.data_root, self.ann_prefix, img_id+'.txt')
-
+   
     def get_txt_ann_info(self, txt_path):
         """Get annotation from TXT file by index.
         Args:
@@ -136,32 +196,7 @@ class CenterCOCODataset(COCO):
             anns.append(ann)
         return anns
 
-    def get_cat_ids(self, idx):
-        """Get category ids in TXT file by index.
-        Args:
-            idx (int): Index of data.
-        Returns:
-            list[int]: All categories in the image of specified index.
-        """
-
-        cat_ids = []
-        # img_path = self.data_infos[idx]['file_name']
-        # txt_path = self.get_ann_path_from_img_path(img_path)
-        txt_path = self.data_infos[idx]['ann_path']
-        txt_path = osp.join(self.data_root, self.ann_prefix, txt_path)
-        if osp.exists(txt_path):
-            with open(txt_path,'r') as fp:
-                lines = fp.readlines()
-        else:
-            lines = []
-
-        for line in lines:
-            obj = [int(x) for x in line.strip().split(',')]
-            # label, xmin, ymin, xmax, ymax = obj
-            cat_ids.append(obj[0])
-
-        return 
-
+    
     def _load_jsons(self):
         _coco = self.coco
         json_id_to_contiguous = {
@@ -311,3 +346,45 @@ class CenterCOCODataset(COCO):
             meta = {'c': c, 's': s, 'gt_det': gt_det, 'img_id': img_id}
             ret['meta'] = meta
         return inp, hm, wh, reg, ind, reg_mask
+    
+    
+    def _to_float(self, x):
+        return float("{:.2f}".format(x))
+
+    def convert_eval_format(self, all_bboxes):
+        detections = []
+        for image_id in all_bboxes:
+          for cls_ind in all_bboxes[image_id]:
+            category_id = self._valid_ids[cls_ind - 1]
+            for bbox in all_bboxes[image_id][cls_ind]:
+              bbox[2] -= bbox[0]
+              bbox[3] -= bbox[1]
+              score = bbox[4]
+              bbox_out  = list(map(self._to_float, bbox[0:4]))
+
+              detection = {
+                  "image_id": int(image_id),
+                  "category_id": int(category_id),
+                  "bbox": bbox_out,
+                  "score": float("{:.2f}".format(score))
+              }
+              if len(bbox) > 5:
+                  extreme_points = list(map(self._to_float, bbox[5:13]))
+                  detection["extreme_points"] = extreme_points
+              detections.append(detection)
+        return detections
+
+      def __len__(self):
+        return self.num_samples
+
+      def save_results(self, results, save_dir):
+        json.dump(self.convert_eval_format(results),
+                    open('{}/results.json'.format(save_dir), 'w'))
+
+      def run_eval(self, results, save_dir):
+        self.save_results(results, save_dir)
+        coco_dets = self.coco.loadRes('{}/results.json'.format(save_dir))
+        coco_eval = COCOeval(self.coco, coco_dets, "bbox")
+        coco_eval.evaluate()
+        coco_eval.accumulate()
+        coco_eval.summarize()
